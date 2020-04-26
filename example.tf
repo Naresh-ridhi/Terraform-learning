3eddc6bb0e0eed00712c514bfc28e25d0d21bfa8
Naresh-ridhi <55054162+Naresh-ridhi@users.noreply.github.com>
2020-04-26 23:42:43 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,9 +1,14 @@
-terraform {
-  backend "remote" {
-    organization = "Accenture2010"
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA4VP6TB5FXJW3BA77"
+  secret_key = "3oEcr4GRP+pEe7FGcDznwK9LQtKF0xD1tM6wBUJk"
+}
 
-    workspaces {
-      name = "Terraform-learning"
-    }
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+tags = {
+    Name = "Terraform-example"
   }
 }
